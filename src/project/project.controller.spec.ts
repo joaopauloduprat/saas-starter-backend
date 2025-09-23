a7ff2c43383fa23f18493cb3410954107a6985c0
joaopauloduprat <duprat.dev@gmail.com>
2025-09-23 20:17:21 -0300

    feat: enhance Project module tests with user context and mock implementations for service methods

--- src/project/project.controller.spec.ts
+++ src/project/project.controller.spec.ts
@@ -1,18 +1,155 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectController } from './project.controller';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { ProjectService } from './project.service';
+import JwtPayload from '../auth/types/jwt-payload';
 
 describe('ProjectController', () => {
-  let controller: ProjectController;
+  let projectController: ProjectController;
+  let projectServiceMock: DeepMockProxy<ProjectService>;
+
+  const mockUser: JwtPayload = {
+    sub: 'user-id-123',
+    email: '',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProjectController],
+      providers: [
+        { provide: ProjectService, useValue: mockDeep<ProjectService>() },
+      ],
     }).compile();
 
-    controller = module.get<ProjectController>(ProjectController);
+    projectController = module.get(ProjectController);
+    projectServiceMock = module.get(ProjectService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(projectController).toBeDefined();
+  });
+
+  it('should create a project for the current user', async () => {
+    const projectDto = {
+      name: 'New Project',
+      description: 'New Project',
+    };
+
+    const result = {
+      id: 'project-id-123',
+      userId: mockUser.sub,
+      ...projectDto,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    projectServiceMock.create.mockResolvedValue(result);
+
+    expect(await projectController.create(mockUser, projectDto)).toEqual(
+      result,
+    );
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(projectServiceMock.create).toHaveBeenCalledWith(
+      mockUser.sub,
+      projectDto,
+    );
+  });
+
+  it('should get all projects for the current user', async () => {
+    const result = [
+      {
+        id: 'project-id-123',
+        userId: mockUser.sub,
+        name: 'Project 1',
+        description: 'Description 1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: 'project-id-124',
+        userId: mockUser.sub,
+        name: 'Project 2',
+        description: 'Description 2',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+
+    projectServiceMock.findAll.mockResolvedValue(result);
+
+    expect(await projectController.findAll(mockUser)).toEqual(result);
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(projectServiceMock.findAll).toHaveBeenCalledWith(mockUser.sub);
+  });
+
+  it('should get a project by id for the current user', async () => {
+    const result = {
+      id: 'user-id-123',
+      userId: mockUser.sub,
+      name: 'Project 1',
+      description: 'Description 1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    projectServiceMock.findOne.mockResolvedValue(result);
+
+    expect(await projectController.findOne(mockUser, 'user-id-123')).toEqual(
+      result,
+    );
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(projectServiceMock.findOne).toHaveBeenCalledWith(
+      mockUser.sub,
+      'user-id-123',
+    );
+  });
+
+  it('should update a project by id for the current user', async () => {
+    const updateDto = {
+      name: 'Updated Project',
+      description: 'Updated Description',
+    };
+
+    const result = {
+      id: 'user-id-123',
+      userId: mockUser.sub,
+      ...updateDto,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    projectServiceMock.update.mockResolvedValue(result);
+
+    expect(
+      await projectController.update(mockUser, 'user-id-123', updateDto),
+    ).toEqual(result);
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(projectServiceMock.update).toHaveBeenCalledWith(
+      mockUser.sub,
+      'user-id-123',
+      updateDto,
+    );
+  });
+
+  it('should delete a project by id for the current user', async () => {
+    const result = {
+      id: 'user-id-123',
+      userId: mockUser.sub,
+      name: 'Project 1',
+      description: 'Description 1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    projectServiceMock.delete.mockResolvedValue(result);
+
+    expect(await projectController.delete(mockUser, 'user-id-123')).toEqual(
+      result,
+    );
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(projectServiceMock.delete).toHaveBeenCalledWith(
+      mockUser.sub,
+      'user-id-123',
+    );
   });
 });
--- src/project/project.service.spec.ts
+++ src/project/project.service.spec.ts
@@ -1,18 +1,164 @@
+import { PrismaClient, Project } from '@prisma/client';
 import { Test, TestingModule } from '@nestjs/testing';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+
 import { ProjectService } from './project.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('ProjectService', () => {
-  let service: ProjectService;
+  let projectService: ProjectService;
+  let prismaMock: DeepMockProxy<PrismaClient>;
+
+  const mockProjects: Project[] = [
+    {
+      id: '1',
+      name: 'Project A',
+      description: 'Desc A',
+      userId: '1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: '2',
+      name: 'Project B',
+      description: 'Desc B',
+      userId: '1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectService],
+      providers: [
+        ProjectService,
+        { provide: PrismaService, useValue: mockDeep<PrismaClient>() },
+      ],
     }).compile();
 
-    service = module.get<ProjectService>(ProjectService);
+    prismaMock = module.get(PrismaService);
+    projectService = module.get(ProjectService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(projectService).toBeDefined();
+  });
+
+  it('should create a project for a user', async () => {
+    const dto = { name: 'New Project', description: 'Test' };
+    const result = {
+      id: '3',
+      ...dto,
+      userId: '1',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    prismaMock.project.create.mockResolvedValue(result);
+
+    expect(await projectService.create('1', dto)).toEqual(result);
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(prismaMock.project.create).toHaveBeenCalledWith({
+      data: { ...dto, userId: '1' },
+    });
+  });
+
+  it('should return all projects for a user', async () => {
+    prismaMock.project.findMany.mockResolvedValue(mockProjects);
+
+    expect(await projectService.findAll('1')).toEqual(mockProjects);
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(prismaMock.project.findMany).toHaveBeenCalledWith({
+      where: { userId: '1' },
+    });
+  });
+
+  it('should return a project by id and userId', async () => {
+    prismaMock.project.findFirst.mockImplementation(
+      (args: any) =>
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        Promise.resolve(
+          mockProjects.find(
+            (project) =>
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+              project.id === args.where.id &&
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+              project.userId === args.where.userId,
+          ),
+        ) as any,
+    );
+
+    expect(await projectService.findOne('1', '1')).toEqual(mockProjects[0]);
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(prismaMock.project.findFirst).toHaveBeenCalledWith({
+      where: { id: '1', userId: '1' },
+    });
+  });
+
+  it('should update a project', async () => {
+    const dto = { name: 'Updated Project' };
+    const updated = { ...mockProjects[0], ...dto };
+
+    prismaMock.project.findFirst.mockImplementation(
+      (args: any) =>
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        Promise.resolve(
+          mockProjects.find(
+            (project) =>
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+              project.id === args.where.id &&
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+              project.userId === args.where.userId,
+          ),
+        ) as any,
+    );
+
+    prismaMock.project.update.mockImplementation(
+      ({ where, data }) =>
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        Promise.resolve({
+          ...mockProjects.find((project) => project.id === where.id)!,
+          ...data,
+        }) as any,
+    );
+
+    expect(await projectService.update('1', '1', dto)).toEqual(updated);
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect(prismaMock.project.update).toHaveBeenCalledWith({
+      where: { id: '1' },
+      data: dto,
+    });
+  });
+
+  describe('remove', () => {
+    it('should delete a project', async () => {
+      prismaMock.project.findFirst.mockImplementation(
+        (args: any) =>
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+          Promise.resolve(
+            mockProjects.find(
+              (project) =>
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                project.id === args.where.id &&
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                project.userId === args.where.userId,
+            ),
+          ) as any,
+      );
+
+      prismaMock.project.delete.mockImplementation(
+        ({ where }) =>
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+          Promise.resolve(
+            mockProjects.find((project) => project.id === where.id)!,
+          ) as any,
+      );
+
+      expect(await projectService.delete('1', '1')).toEqual(mockProjects[0]);
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      expect(prismaMock.project.delete).toHaveBeenCalledWith({
+        where: { id: '1' },
+      });
+    });
   });
 });
